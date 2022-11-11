7c56abb887e3d9338056a50c765befbbc4f7c15b
LinTzuAn <a22341254@gmail.com>
2022-11-11 17:17:30 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-// require package
 const express = require('express')
-const app = express()
-const port = 3000
-
-// require handlebars
 const exphbs = require('express-handlebars')
+const port = 3000
+const mongoose = require('mongoose')
+const app = express()
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const restaurantList = require('./restaurant.json')
 
-// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting static files
+
 app.use(express.static('public'))
 
-// routes setting
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
@@ -41,7 +53,6 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: filteredRestaurant })
 })
 
-// start and listen 
 app.listen(port, () => {
   console.log(`Express is running on localhost: ${port}`)
 })
