29a7dc61247c3ff51b683d43c9e3cc2964279e2f
LinTzuAn <a22341254@gmail.com>
2022-11-17 22:47:14 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,9 +4,20 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
+  const mode = req.query.sorting
+  const sorting = {
+    default: {_id: 'asc'},
+    AtoZ: { name: 'asc' },
+    ZtoA: { name: 'desc' },
+    category: { category: 'asc' },
+    location: { location: 'asc' }
+  }
+  const selected = {[mode]: true}
+
   Restaurant.find()
     .lean()
-    .then(restaurants => res.render('index', { restaurants }))
+    .sort(sorting[mode])
+    .then(restaurants => res.render('index', { restaurants, selected }))
     .catch(error => console.error(error))
 })
 
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,21 +4,31 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  if (!req.query.keyword) {
-    res.redirect("/")
-  }
-
   const keyword = req.query.keyword.trim().toLowerCase().replace(/ /g, '')
+  const mode = req.query.sorting
+  const sorting = {
+    default: { _id: 'asc' },
+    AtoZ: { name: 'asc' },
+    ZtoA: { name: 'desc' },
+    category: { category: 'asc' },
+    location: { location: 'asc' }
+  }
+  const sortSelected = { [mode]: true }
+  
+  if (!req.query.keyword) {
+    return res.redirect("/")
+  }
 
   Restaurant.find()
     .lean()
+    .sort(sorting[mode])
     .then(
       restaurants => {
         const filteredRestaurant = restaurants.filter(
           item => item.name.toLowerCase().includes(keyword) ||
             item.category.includes(keyword)
         )
-        res.render('index', { restaurants: filteredRestaurant, keyword })
+        res.render('index', { restaurants: filteredRestaurant, keyword, sortSelected })
       })
     .catch(error => console.log(error))
 })
