26590ea48ad996760654e6269a3b5db76bc7f887
LinTzuAn <a22341254@gmail.com>
2023-02-11 15:47:46 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const port = 3000
 
 const methodOverride = require('method-override')
-
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -26,10 +26,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error_msg = req.flash('error')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,7 +10,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -20,16 +21,35 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // get user registration info 
   const { email, name, password, confirmPassword } = req.body
+  // check info
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'Please fill in all the blanks.' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'Passwords do not match.' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   // check whether user has already register 
   User.findOne({ email })
   .then(user => {
     // if true, return to previous page else save user info
     if (user) {
+      errors.push({ message: 'This email address has been taken.' })
       res.render('register', {
         email,
         name,
         password,
-        confirmPassword
+        confirmPassword,
+        errors
       })
     } else {
       return bcrypt
@@ -49,6 +69,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', 'You have been logged out successfully.')
   res.redirect('/users/login')
 })
 
