a39ed29209956b7ae0f553a88694e880cfbd72f1
LinTzuAn <a22341254@gmail.com>
2023-02-11 12:20:37 +0800

    feat: add user model and modified restaurant model

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    defalut: Date.now
+  }
+})
+module.exports = mongoose.model('User', userSchema)
